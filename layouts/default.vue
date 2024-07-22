3fa5424a33205cdc26fb01ccbcd60a0d88fc1e4b
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-07-22 14:44:55 +0300

    Add a default layout

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex w-full flex-col">
+    <Header />
+    <Navbar />
+    <Breadcrumb />
+    <main class="flex w-full gap-10 px-12">
+      <slot />
+    </main>
+  </div>
+</template>
