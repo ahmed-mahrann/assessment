e1c2f08d123be655136d2bb6a4f98a06b7ba919c
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-07-22 15:02:41 +0300

    Change component names to be up to standards

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex w-full flex-col">
-    <Header />
-    <Navbar />
-    <Breadcrumb />
+    <app-header />
+    <navbar />
+    <breadcrumb />
     <main class="flex w-full gap-10 px-12">
       <slot />
     </main>
