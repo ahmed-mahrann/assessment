6c259001ee868ff913b3b36cf3ae7a6d28f11680
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-07-22 13:44:15 +0300

    Setup project

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { register as registerSwiper } from "swiper/element/bundle";
+
+registerSwiper();
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        title: "#1B1C1E",
+        subtitle: "#C1C1C1",
+        surface: "#FBFBFB",
+        buttonBg: "#00ABEB",
+        inputBg: "#F3F3F3",
+        grayBg: "#F5F5F5",
+      },
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".scrollbar-hidden::-webkit-scrollbar": {
+          display: "none",
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
+};
