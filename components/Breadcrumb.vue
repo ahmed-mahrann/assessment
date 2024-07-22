6ab370e4c56fd5d7c0a971b185a88af275ddf822
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-07-22 14:43:59 +0300

    Create Breadcrumb component

---
+++ components/Breadcrumb.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex gap-2 border-t px-6 py-5 text-[13px] sm:px-12">
+    <common-button text="Home" type="link" />
+    <span class="text-subtitle">/</span>
+    <common-button text="Hoodies" type="link" />
+    <span class="text-subtitle">/</span>
+    <common-button
+      text="Hoodie - Dolce & Banana"
+      type="link"
+      class="text-subtitle"
+    />
+  </div>
+</template>
