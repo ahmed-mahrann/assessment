1356be9dd4f4254aa94decab6f6280a108e93638
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-07-22 14:44:34 +0300

    Create button and searchbar common components

---
+++ components/common/button.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+  },
+  icon: {
+    type: String,
+  },
+  type: {
+    type: String as PropType<"solid" | "outline" | "link">,
+    default: "solid",
+  },
+  disable: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const typeClass = {
+  solid: "bg-buttonBg text-white hover:bg-buttonBg/80",
+  outline: "text-buttonBg border border-buttonBg hover:bg-buttonBg/20",
+  link: "hover:opacity-70",
+};
+</script>
+
+<template>
+  <button
+    class="flex items-center gap-2 rounded-md disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:opacity-30"
+    :class="typeClass[type]"
+    :disabled="disable"
+    @click="$emit('click')"
+  >
+    <img v-if="icon" :src="icon" alt="icon" :class="typeClass.link" />
+    <span v-if="text" :class="icon && text ? 'hidden md:inline' : ''">
+      {{ text }}
+    </span>
+    <slot />
+  </button>
+</template>
---
+++ components/common/searchbar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import searchIcon from "~/assets/icons/search-icon.svg";
+
+const searchValue = defineModel("value", {
+  type: String,
+});
+
+defineProps({
+  placeholderTitle: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div class="flex w-1/2 items-center gap-0.5 rounded-md bg-inputBg px-3 py-2">
+    <img class="mr-2 opacity-30" :src="searchIcon" />
+    <input
+      v-model="searchValue"
+      type="text"
+      :placeholder="
+        placeholderTitle ? `Search by ${placeholderTitle}` : 'Search...'
+      "
+      class="border-none bg-transparent outline-none"
+    />
+  </div>
+</template>
