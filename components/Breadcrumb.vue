599aa478ab813563cc5a45bd6a05cb2d3d0caa1d
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-07-25 19:07:17 +0300

    Modify Breadcrumb to be dynamic

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -2,12 +2,21 @@
   <div class="flex gap-2 border-t px-6 py-5 text-[13px] sm:px-12">
     <common-button text="Home" type="link" />
     <span class="text-subtitle">/</span>
-    <common-button text="Hoodies" type="link" />
+    <common-button
+      :text="product?.categories?.[0].title || 'Sub Category'"
+      type="link"
+    />
     <span class="text-subtitle">/</span>
     <common-button
-      text="Hoodie - Dolce & Banana"
+      :text="product?.title || 'Product'"
       type="link"
       class="text-subtitle"
     />
   </div>
 </template>
+
+<script setup lang="ts">
+import { useProductStore } from "~/store/productStore";
+
+const { productData: product } = useProductStore();
+</script>
