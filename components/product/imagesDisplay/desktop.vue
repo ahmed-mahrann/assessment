1541e196c254765a5f00e875b8930f3ad22b35c4
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-07-27 11:54:25 +0300

    Implement desktop images view

---
+++ components/product/imagesDisplay/desktop.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex h-full items-start gap-6">
+    <div class="flex flex-col gap-4">
+      <template v-for="(image, index) in slides" :key="image">
+        <common-image
+          class="rounded-auto flex h-20 w-20 items-center justify-center overflow-hidden"
+          img-class="!object-contain unset-size h-auto w-auto max-w-full max-h-full"
+          alt="product-image-thumbnail"
+          :src="image"
+          :class="{
+            'outline outline-2 outline-black': currentSlideIndex === index,
+          }"
+          @click="$emit('select', index)"
+        />
+      </template>
+    </div>
+    <common-image
+      loaderClass="size-96"
+      img-class="!object-contain"
+      :src="slides[currentSlideIndex]"
+      :alt="product?.title"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useProductStore } from "~/store/productStore";
+
+defineProps({
+  slides: {
+    type: Object as PropType<string[]>,
+    required: true,
+  },
+  currentSlideIndex: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const { productData: product } = useProductStore();
+</script>
